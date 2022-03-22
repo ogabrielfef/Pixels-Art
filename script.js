2c4345998204fa916442880967b78c02c692e03b
GabrielF <ogabrielfef@gmail.com>
2022-03-22 11:45:15 -0300

    Requisitos 5-6

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//
+
+function pixelBoard(){
+    let cont = document.getElementById("pixel-board");
+    cont.style.display = "block";
+    cont.style.width = "210px";
+    cont.style.height = "210px";
+
+    for ( let i = 0; i < 25; i++){
+       let quadro = document.createElement("div");
+        quadro.style.marginBottom = '-5px'
+        quadro.className = "pixel";
+        quadro.style.border = "solid black 1px"
+        quadro.style.width = "40px"
+        quadro.style.height = "40px"
+        quadro.style.backgroundColor = "white"
+        quadro.style.display = "inline-block"
+   cont.appendChild(quadro);
+   }
+}
+pixelBoard();
